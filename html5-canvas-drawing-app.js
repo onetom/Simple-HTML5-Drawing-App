4cb9fe7781242c9680e89a5bc77d3a806ff034fc
Stephen Davidson <davidson.sc19@gmail.com>
2017-11-05 22:34:46 -0600

    Make this dead simple

--- html5-canvas-drawing-app.js
+++ html5-canvas-drawing-app.js
@@ -1,24 +1,16 @@
-// TODO: Rename EVERYTHING
 var drawingApp = (function () {
-
-	"use strict";
-
 	var canvas,
 		context,
-		canvasWidth = 490,
-		canvasHeight = 220,
-		colorPurple = "#cb3594",
-		colorGreen = "#659b41",
-		colorYellow = "#ffcf33",
-		colorBrown = "#986928",
-		outlineImage = new Image(),
-		crayonImage = new Image(),
-		markerImage = new Image(),
-		eraserImage = new Image(),
-		crayonBackgroundImage = new Image(),
-		markerBackgroundImage = new Image(),
-		eraserBackgroundImage = new Image(),
-		crayonTextureImage = new Image(),
+		canvasWidth = 980,
+		canvasHeight = 440,
+		colorBlack = "#000000",
+		colorBlue = "#0000ff",
+		colorGreen = "#00c142",
+		colorRed = "#ff0000",
+		brushHuge = 20,
+		brushLarge = 10,
+		brushNormal = 5,
+		brushSmall = 2,
 		clickX = [],
 		clickY = [],
 		clickColor = [],
@@ -26,261 +18,118 @@ var drawingApp = (function () {
 		clickSize = [],
 		clickDrag = [],
 		paint = false,
-		curColor = colorPurple,
-		curTool = "crayon",
-		curSize = "normal",
-		mediumStartX = 18,
-		mediumStartY = 19,
-		mediumImageWidth = 93,
-		mediumImageHeight = 46,
-		drawingAreaX = 111,
-		drawingAreaY = 11,
-		drawingAreaWidth = 267,
-		drawingAreaHeight = 200,
-		toolHotspotStartY = 23,
-		toolHotspotHeight = 38,
-		sizeHotspotStartY = 157,
-		sizeHotspotHeight = 36,
-		totalLoadResources = 8,
-		curLoadResNum = 0,
-		sizeHotspotWidthObject = {
-			huge: 39,
-			large: 25,
-			normal: 18,
-			small: 16
-		},
+		currentColor = colorBlack,
+		currentTool = "marker",
+		currentSize = brushNormal,
 
-		// Clears the canvas.
-		clearCanvas = function () {
-			context.clearRect(0, 0, canvasWidth, canvasHeight);
-		},
-
-		// Redraws the canvas.
 		redraw = function () {
-			var locX, locY, radius, i, selected,
+			context.clearRect(0, 0, canvas.width, canvas.height);
 
-				drawCrayon = function (x, y, color, selected) {
-					context.drawImage(crayonImage, 0, 0, 59, mediumImageHeight, x, y, 59, mediumImageHeight);
-				},
-
-				drawMarker = function (x, y, color, selected) {
-					context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, x, y, 59, mediumImageHeight);
-				};
-
-			// Make sure required resources are loaded before redrawing
-			if (curLoadResNum < totalLoadResources) {
-				return;
-			}
-
-			clearCanvas();
-
-			if (curTool === "crayon") {
-				// Draw the crayon tool background
-				context.drawImage(crayonBackgroundImage, 0, 0, canvasWidth, canvasHeight);
-			} else if (curTool === "marker") {
-				// Draw the marker tool background
-				context.drawImage(markerBackgroundImage, 0, 0, canvasWidth, canvasHeight);
-			} else if (curTool === "eraser") {
-				// Draw the eraser tool background
-				context.drawImage(eraserBackgroundImage, 0, 0, canvasWidth, canvasHeight);
-			}
-
-			// Keep the drawing in the drawing area
-			context.save();
-			context.beginPath();
-			context.rect(drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
-			context.clip();
-
-			// For each point drawn
 			for (i = 0; i < clickX.length; i += 1) {
-
-				// Set the drawing radius
-				switch (clickSize[i]) {
-					case "small":
-						radius = 2;
-						break;
-					case "normal":
-						radius = 5;
-						break;
-					case "large":
-						radius = 10;
-						break;
-					case "huge":
-						radius = 20;
-						break;
-					default:
-						break;
-				}
-
-				// Set the drawing path
 				context.beginPath();
-				// If dragging then draw a line between the two points
-				if (clickDrag[i] && i) {
+				if (clickDrag[i] && i > 0) {
 					context.moveTo(clickX[i - 1], clickY[i - 1]);
 				} else {
-					// The x position is moved over one pixel so a circle even if not dragging
 					context.moveTo(clickX[i] - 1, clickY[i]);
 				}
 				context.lineTo(clickX[i], clickY[i]);
 
-				// Set the drawing color
-				if (clickTool[i] === "eraser") {
-					context.strokeStyle = 'white';
-				} else {
-					context.strokeStyle = clickColor[i];
-				}
+				context.strokeStyle = (clickTool[i] === "eraser") ? 'white' : clickColor[i];
 				context.lineCap = "round";
 				context.lineJoin = "round";
-				context.lineWidth = radius;
+				context.lineWidth = clickSize[i];
+
 				context.stroke();
 			}
 			context.closePath();
 			context.restore();
-			context.globalAlpha = 1; // No IE support
 		},
 
-		// Adds a point to the drawing array.
 		addClick = function (x, y, dragging) {
 			clickX.push(x);
 			clickY.push(y);
-			clickTool.push(curTool);
-			clickColor.push(curColor);
-			clickSize.push(curSize);
+			clickTool.push(currentTool);
+			clickColor.push(currentColor);
+			clickSize.push(currentSize);
 			clickDrag.push(dragging);
 		},
 
-		// Add mouse and touch event listeners to the canvas
-		createUserEvents = function () {
+		setupListeners = function () {
+			document.getElementById("marker").addEventListener("click", function () {
+				currentTool = "marker";
+			});
+			document.getElementById("eraser").addEventListener("click", function () {
+				currentTool = "eraser"
+			});
+			document.getElementById("black").addEventListener("click", function () {
+				currentColor = colorBlack;
+			});
+			document.getElementById("blue").addEventListener("click", function () {
+				currentColor = colorBlue;
+			});
+			document.getElementById("green").addEventListener("click", function () {
+				currentColor = colorGreen;
+			});
+			document.getElementById("red").addEventListener("click", function () {
+				currentColor = colorRed;
+			});
+			document.getElementById("huge").addEventListener("click", function () {
+				currentSize = brushHuge;
+			});
+			document.getElementById("large").addEventListener("click", function () {
+				currentSize = brushLarge;
+			});
+			document.getElementById("normal").addEventListener("click", function () {
+				currentSize = brushNormal;
+			});
+			document.getElementById("small").addEventListener("click", function () {
+				currentSize = brushSmall;
+			});
 
 			var press = function (e) {
-				// Mouse down location
-				var sizeHotspotStartX,
-					mouseX = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - this.offsetLeft,
-					mouseY = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - this.offsetTop;
+				var mouseX = e.pageX - this.offsetLeft,
+					mouseY = e.pageY - this.offsetTop;
 
-				if (mouseX < drawingAreaX) { // Left of the drawing area
-					if (mouseX > mediumStartX) {
-						if (mouseY > mediumStartY && mouseY < mediumStartY + mediumImageHeight) {
-							curColor = colorPurple;
-						} else if (mouseY > mediumStartY + mediumImageHeight && mouseY < mediumStartY + mediumImageHeight * 2) {
-							curColor = colorGreen;
-						} else if (mouseY > mediumStartY + mediumImageHeight * 2 && mouseY < mediumStartY + mediumImageHeight * 3) {
-							curColor = colorYellow;
-						} else if (mouseY > mediumStartY + mediumImageHeight * 3 && mouseY < mediumStartY + mediumImageHeight * 4) {
-							curColor = colorBrown;
-						}
-					}
-				} else if (mouseX > drawingAreaX + drawingAreaWidth) { // Right of the drawing area
-
-					if (mouseY > toolHotspotStartY) {
-						if (mouseY > sizeHotspotStartY) {
-							sizeHotspotStartX = drawingAreaX + drawingAreaWidth;
-							if (mouseY < sizeHotspotStartY + sizeHotspotHeight && mouseX > sizeHotspotStartX) {
-								if (mouseX < sizeHotspotStartX + sizeHotspotWidthObject.huge) {
-									curSize = "huge";
-								} else if (mouseX < sizeHotspotStartX + sizeHotspotWidthObject.large + sizeHotspotWidthObject.huge) {
-									curSize = "large";
-								} else if (mouseX < sizeHotspotStartX + sizeHotspotWidthObject.normal + sizeHotspotWidthObject.large + sizeHotspotWidthObject.huge) {
-									curSize = "normal";
-								} else if (mouseX < sizeHotspotStartX + sizeHotspotWidthObject.small + sizeHotspotWidthObject.normal + sizeHotspotWidthObject.large + sizeHotspotWidthObject.huge) {
-									curSize = "small";
-								}
-							}
-						} else {
-							if (mouseY < toolHotspotStartY + toolHotspotHeight) {
-								curTool = "crayon";
-							} else if (mouseY < toolHotspotStartY + toolHotspotHeight * 2) {
-								curTool = "marker";
-							} else if (mouseY < toolHotspotStartY + toolHotspotHeight * 3) {
-								curTool = "eraser";
-							}
-						}
-					}
-				}
 				paint = true;
 				addClick(mouseX, mouseY, false);
 				redraw();
-			},
-				drag = function (e) {
-					var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - this.offsetLeft,
-						mouseY = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - this.offsetTop;
+			};
+			var drag = function (e) {
+				var mouseX = e.pageX - this.offsetLeft,
+					mouseY = e.pageY - this.offsetTop;
 
-					if (paint) {
-						addClick(mouseX, mouseY, true);
-						redraw();
-					}
-					// Prevent the whole page from dragging if on mobile
-					e.preventDefault();
-				},
-
-				release = function () {
-					paint = false;
+				if (paint) {
+					addClick(mouseX, mouseY, true);
 					redraw();
-				},
+				}
+			};
+			var release = function () {
+				paint = false;
+				redraw();
+			};
+			var cancel = function () {
+				paint = false;
+			};
 
-				cancel = function () {
-					paint = false;
-				};
-
-			// Add mouse event listeners to canvas element
 			canvas.addEventListener("mousedown", press, false);
 			canvas.addEventListener("mousemove", drag, false);
 			canvas.addEventListener("mouseup", release);
 			canvas.addEventListener("mouseout", cancel, false);
-
-			// Add touch event listeners to canvas element
-			canvas.addEventListener("touchstart", press, false);
-			canvas.addEventListener("touchmove", drag, false);
-			canvas.addEventListener("touchend", release, false);
-			canvas.addEventListener("touchcancel", cancel, false);
 		},
 
-		// Calls the redraw function after all neccessary resources are loaded.
-		resourceLoaded = function () {
-			curLoadResNum += 1;
-			if (curLoadResNum === totalLoadResources) {
-				redraw();
-				createUserEvents();
-			}
-		},
-
-		// Creates a canvas element, loads images, adds events, and draws the canvas for the first time.
 		init = function () {
-			// Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
-			canvas = document.createElement('canvas');
+			canvas = document.getElementById('canvas');
 			canvas.setAttribute('width', canvasWidth);
 			canvas.setAttribute('height', canvasHeight);
 			canvas.setAttribute('id', 'canvas');
-			document.getElementById('canvasDiv').appendChild(canvas);
+			canvas.style.border = "1px solid black";
 			if (typeof G_vmlCanvasManager !== "undefined") {
 				canvas = G_vmlCanvasManager.initElement(canvas);
 			}
-			context = canvas.getContext("2d"); // Grab the 2d canvas context
+			context = canvas.getContext("2d");
 
-			// Load images
-			crayonImage.onload = resourceLoaded;
-			crayonImage.src = "images/crayon-outline.png";
-
-			markerImage.onload = resourceLoaded;
-			markerImage.src = "images/marker-outline.png";
-
-			eraserImage.onload = resourceLoaded;
-			eraserImage.src = "images/eraser-outline.png";
-
-			crayonBackgroundImage.onload = resourceLoaded;
-			crayonBackgroundImage.src = "images/crayon-background.png";
-
-			markerBackgroundImage.onload = resourceLoaded;
-			markerBackgroundImage.src = "images/marker-background.png";
-
-			eraserBackgroundImage.onload = resourceLoaded;
-			eraserBackgroundImage.src = "images/eraser-background.png";
-
-			crayonTextureImage.onload = resourceLoaded;
-			crayonTextureImage.src = "images/crayon-texture.png";
-
-			outlineImage.onload = resourceLoaded;
-			outlineImage.src = "images/watermelon-duck-outline.png";
+			redraw();
+			setupListeners();
 		};
 
 	return {
